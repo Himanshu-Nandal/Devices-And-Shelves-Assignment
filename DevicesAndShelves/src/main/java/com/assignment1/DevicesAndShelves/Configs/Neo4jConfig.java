78f6cfe95d8701ff60542a76c0ef7a8adf6efdd5
Nandal <hnandal@ciena.com>
2026-02-24 18:46:00 +0530

    Drivers Debugged

--- DevicesAndShelves/src/main/java/com/assignment1/DevicesAndShelves/Configs/Neo4jConfig.java
+++ DevicesAndShelves/src/main/java/com/assignment1/DevicesAndShelves/Configs/Neo4jConfig.java
@@ -2,22 +2,37 @@ package com.assignment1.DevicesAndShelves.Configs;
 
 import lombok.Value;
 import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.sql.Driver;
-
 // config/Neo4jConfig.java
 @Configuration
 public class Neo4jConfig {
-    @Value("${NEO4J_URI}") private String uri;
-    @Value("${NEO4J_USERNAME}") private String username;
-    @Value("${NEO4J_PASSWORD}") private String password;
+    final private String uri = "${NEO4J_URI}";
+    final private String username = "${NEO4J_USERNAME}";
+    final private String password = "${NEO4J_PASSWORD}";
 //    @Value("${neo4j.password}") private String password;
 
     @Bean(destroyMethod = "close")
     public Driver neo4jDriver() {
-        return GraphDatabase.driver(uri, AuthTokens.basic(username, password));
+        try(org.neo4j.driver.Driver Driver = GraphDatabase.driver(uri, AuthTokens.basic(username, password))){
+            return Driver;
+        }
     }
+
+    // OR
+
+//    public Driver neo4jDriver() {
+//        try (var driver = GraphDatabase.driver(
+//                System.getProperty("NEO4J_URI"), // (1)
+//                AuthTokens.basic(
+//                        System.getProperty("NEO4J_USERNAME"), // (2)
+//                        System.getProperty("NEO4J_PASSWORD"))
+//        )
+//        ) {
+//            return driver;
+//        }
+//    }
 }
